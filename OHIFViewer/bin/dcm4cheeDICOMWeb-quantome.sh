3d39023e2455655c15a1b18dc8443b3dfedd8e2d
Steve Pieper <pieper@isomics.com>
2018-05-31 17:19:28 -0400

    Add re-install step to script to handle case of changing branches with different dependencies

--- OHIFViewer/bin/dcm4cheeDICOMWeb-quantome.sh
+++ OHIFViewer/bin/dcm4cheeDICOMWeb-quantome.sh
@@ -1,2 +1,3 @@
 echo "Starting Meteor server..."
+METEOR_PACKAGE_DIRS="../Packages" meteor npm install
 METEOR_PACKAGE_DIRS="../Packages" meteor --settings ../config/dcm4cheeDICOMWeb-quantome.org.json
